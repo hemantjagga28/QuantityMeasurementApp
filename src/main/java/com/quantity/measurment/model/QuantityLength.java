b8a7912b90e56d7c389a8e74e89b4c2b08514b4b
Hemant Jagga <hemantjagga28@gmail.com>
2026-03-30 11:43:44 +0530

    UC3 IMPLEMENTED

---
+++ src/main/java/com/quantity/measurment/model/QuantityLength.java
@@ -0,0 +1,32 @@
+package com.quantity.measurment.model;
+
+import com.quantity.measurment.enums.LengthUnit;
+
+public class QuantityLength {
+    private final double value;
+    private final LengthUnit unit;
+
+    private QuantityLength(double value, LengthUnit unit){
+
+        if (unit==null) throw new IllegalArgumentException("Unit must not be null");
+        this.value = value;
+        this.unit = unit;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        QuantityLength that = (QuantityLength) obj;
+
+        double thisInFeet = this.unit.toFeet(this.value);
+        double otherInFeet = that.unit.toFeet(that.value);
+
+        return Double.compare(thisInFeet, otherInFeet) == 0;
+
+    }
+}
