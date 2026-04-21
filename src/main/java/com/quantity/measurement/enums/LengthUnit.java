f15d30fb813144553ec954e7fa6475b81700751d
Hemant Jagga <hemantjagga28@gmail.com>
2026-04-21 14:58:59 +0530

    UC9:weight measurement

--- src/main/java/com/quantity/measurement/enums/LengthUnit.java
+++ src/main/java/com/quantity/measurement/enums/LengthUnit.java
@@ -14,16 +14,20 @@ public enum LengthUnit {
     }
 
     public double convertToBaseUnit(double value) {
+
         if (!Double.isFinite(value)) {
             throw new IllegalArgumentException("Invalid value");
         }
+
         return value * toFeetFactor;
     }
 
     public double convertFromBaseUnit(double value) {
+
         if (!Double.isFinite(value)) {
             throw new IllegalArgumentException("Invalid value");
         }
+
         return value / toFeetFactor;
     }
 }
